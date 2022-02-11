7d2533db04f2d255f9b17853b6abe297e8230a7d
ZeynebKoc <koczeyneb@gmail.com>
2022-02-11 14:57:59 +0300

    Create style file of HomePage Component

--- src/pages/HomePage/homepage.component.jsx
+++ src/pages/HomePage/homepage.component.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import srcbgHomePage from "../../assets/images-homepage/bg-homePage.png";
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <React.Fragment>
+                <div
+                    class="srcbgHomePage"
+                    style={{
+                        backgroundImage: "url(" + srcbgHomePage + ")",
+                    }}
+                >
+                    <div className="nav">
+                        <Link to="home" className="home">
+                            Home
+                        </Link>
+                        <Link to="/" className="logOut">
+                            Log Out
+                        </Link>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 }
