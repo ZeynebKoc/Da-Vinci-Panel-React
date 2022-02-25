3a92d2be04b31d90bcfd16c0a781299e630ed632
ZeynebKoc <koczeyneb@gmail.com>
2022-02-25 17:33:46 +0300

    Combine reducers for Store

---
+++ src/redux/reducers/root-Reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import getDateTimeReducer from "./getDateTimeReducer";
+
+const rootReducer = combineReducers({
+    getDateTimeReducer,
+});
+
+export default rootReducer;
