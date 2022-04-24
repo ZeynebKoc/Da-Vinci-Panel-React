850c869018e2283bcb53f2b7b9bda748f0bb5c01
ZeynebKoc <koczeyneb@gmail.com>
2022-04-25 00:45:39 +0300

    Create finishTime reducer and Add to root reducer

--- src/redux/reducers/root-Reducer.js
+++ src/redux/reducers/root-Reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import getDateReducer from "./getDateReducer";
 import getStartTimeReducer from "./getStartTimeReducer";
+import getFinishTimeReducer from "./getFinishTimeReducer";
 
 const rootReducer = combineReducers({
     getDateReducer,
     getStartTimeReducer,
+    getFinishTimeReducer,
 });
 
 export default rootReducer;
