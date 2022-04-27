7018b4dfe3aafdea0b1052580101b5d9ddbf81ed
ZeynebKoc <koczeyneb@gmail.com>
2022-04-27 17:15:12 +0300

    Create createTableReducer and Add to root reducer

---
+++ src/redux/reducers/createTableReducer.js
@@ -0,0 +1,28 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const INITIAL_STATE = {
+    table: {
+        tableName: "",
+        startTime: new Date().toLocaleTimeString("en-GB", {
+            hour: "numeric",
+            minute: "numeric",
+        }),
+        finishTime: "",
+        playerCount: "",
+        currentGame: "",
+    },
+};
+
+export default function createTableReducer(
+    state = INITIAL_STATE.table,
+    action
+) {
+    switch (action.type) {
+        case actionTypes.GET_CREATE_TABLE:
+            return state;
+        case actionTypes.SET_CREATE_TABLE:
+            return action.payload;
+        default:
+            return state;
+    }
+}
--- src/redux/reducers/root-Reducer.js
+++ src/redux/reducers/root-Reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import getDateReducer from "./getDateReducer";
 import tablesCountReducer from "./tablesCountReducer";
+import createTableReducer from "./createTableReducer";
 
 const rootReducer = combineReducers({
     getDateReducer,
     tablesCountReducer,
+    createTableReducer,
 });
 
 export default rootReducer;
